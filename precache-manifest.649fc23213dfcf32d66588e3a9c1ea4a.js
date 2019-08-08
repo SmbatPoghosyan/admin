aac616591796f0ca91f83817ede49dc83233ca87
smbatpoghosyan <smbat.poghosyan@polymorphic.io>
2019-08-08 18:30:40 +0400

    Updates

---
+++ precache-manifest.649fc23213dfcf32d66588e3a9c1ea4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5246f8de24cc74c82ba10acd1f30f2c1",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "9d22509f00e935936586",
+    "url": "/admin/static/css/2.e7ec537b.chunk.css"
+  },
+  {
+    "revision": "964c806500e1cd077a7b",
+    "url": "/admin/static/css/main.cf31a0d5.chunk.css"
+  },
+  {
+    "revision": "9d22509f00e935936586",
+    "url": "/admin/static/js/2.7b9a8d72.chunk.js"
+  },
+  {
+    "revision": "964c806500e1cd077a7b",
+    "url": "/admin/static/js/main.f7f52183.chunk.js"
+  },
+  {
+    "revision": "7c4f45329e47798217a3",
+    "url": "/admin/static/js/runtime~main.b3695f6e.js"
+  }
+]);
